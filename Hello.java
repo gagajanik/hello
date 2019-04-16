e6086119373f48ca61955f711c86de856d94eebc
gjanikashvili <gjanikashvili@silknet.com>
2019-04-16 15:41:18 +0400

    third

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
 	System.out.println("gari janikashvili");
-	System.out.println("rame");
+	System.out.println("rame bla bla");
     }
 }
