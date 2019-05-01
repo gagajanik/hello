fb1043d27354e0107978b5ea64ee6db5e33140ff
gjanikashvili <gjanikashvili@silknet.com>
2019-05-01 10:19:59 +0400

    satesto

--- Hello.java
+++ Hello.java
@@ -1,7 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello world");
-	System.out.println("gari janikashvili");
-	System.out.println("rame bla bla");
+        System.out.println("gamarjoba");
     }
 }
